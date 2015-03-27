c5568cd5ca7f7d7847919462484b513d65d62a21
Varun <mvresh@gmail.com>
2015-03-27 12:34:59 +0530

    Update bio_informatics_easy.py

--- bio_informatics_easy.py
+++ bio_informatics_easy.py
@@ -2,7 +2,7 @@ dic = {'A':'T','T':'A','G':'C','C':'G'}
 string = raw_input('give sequence')
 new = ''
 for char in string:
-    if char in dic.keys():
+    if char in dic:
         new += dic[char]
     else: new += ' ERROR '
 
