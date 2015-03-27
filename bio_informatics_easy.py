4775ea7874c42fee21ebde2dc475f42abdd3ca3f
Varun <mvresh@gmail.com>
2015-03-27 12:31:05 +0530

    Create bio_informatics_easy.py
    DNA Replication

---
+++ bio_informatics_easy.py
@@ -0,0 +1,9 @@
+dic = {'A':'T','T':'A','G':'C','C':'G'}
+string = raw_input('give sequence')
+new = ''
+for char in string:
+    if char in dic.keys():
+        new += dic[char]
+    else: new += ' ERROR '
+
+print new
